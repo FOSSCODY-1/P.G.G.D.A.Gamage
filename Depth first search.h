99ec187fc1a13be75bac4a63087e7bb7c0e9588b
DulangiAnuththara <43141556+DulangiAnuththara@users.noreply.github.com>
2018-09-28 03:14:54 +0530

    new commit 3

--- Depth first search.h
+++ Depth first search.h
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include "Breadth first search.h"
 
 int time=0;
 
 void visit(struct node array[], struct node *ptr){
+	
+	struct edge *temp=ptr->next;
+	
 	if(ptr->color!=0){
 		return;
 	}
@@ -10,15 +14,24 @@ void visit(struct node array[], struct node *ptr){
 		time=time+1;
 		ptr->dtime=time;
 		ptr->color=1;
-		for(int j=0;j<n;j++){
-			visit(array[],);
+		
+		while(temp){
+			
+		visit(array,array+(temp->data-1));
+		temp=temp->next;
+		
+	}
+		time++;
+		ptr->ftime=time;
+		printf("%d\n",ptr->data);
+		return;
+			
+		
+		
 		}
 		
 		
 	}
-	
-}
-
 
 
 
@@ -26,7 +39,7 @@ void DFS(struct node array[],int n){
 	
 	for(int i=0;i<n;i++){
 		if(array[i].color==0){
-			visit(struct node array[],array+i);
+			visit(array,array+i);
 		}
 	}
 }
