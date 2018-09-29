04069d340ffef018ae277650fd92de30cb40a39d
DulangiAnuththara <43141556+DulangiAnuththara@users.noreply.github.com>
2018-09-29 13:20:22 +0530

    add files 3 commit

---
+++ Graph/Depth first search.h
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "Breadth first search.h"
+
+int time=0;
+
+void visit(struct node array[], struct node *ptr){
+	
+	struct edge *temp=ptr->next;
+	
+	if(ptr->color!=0)
+	{
+		return;
+	}
+	else
+	{
+		time=time+1;
+		ptr->dtime=time;
+		ptr->color=1;
+		
+		while(temp)
+		{
+			
+		visit(array,array+(temp->data-1));
+		temp=temp->next;
+		
+		}
+		time++;
+		ptr->ftime=time;
+		printf("\n%d",ptr->data);
+		return;
+			
+		
+		
+		}
+		
+		
+	}
+
+
+
+void DFS(struct node array[],int n){
+	for(int j=0;j<20;j++)
+	{
+		
+		array[j].color=0;
+	}
+	 
+	
+	for(int i=0;i<n;i++)
+	{
+		if(array[i].color==0)
+		{
+			visit(array,array+i);
+		}
+	}
+}
+
